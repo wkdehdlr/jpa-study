9a9e2d3e07d9a50bc634cb473dd2ed9c2b5ce1ba
wkdehdlr <wkdehdlr@gmail.com>
2020-03-08 16:37:28 +0900

    * JPA 구동 방식     * 1. 설정 정보 조회         * META-INF/persistence.xml     * 2. 생성         * EntityManagerFactory     * 3. 생성         * EntityManager
    * 어플리케이션 개발
        * @Id
            * 최소한 JPA 한테 PK가 뭔지는 알려줘야함
        * EntityManagerFactory는 어플리케이션 로딩시점에 딱 하나만 만든다
        * 실제 트랜잭션단위마다 EntityManager를 만들어야함
        * JPA 모든 데이터변경은 트랜잭션안에서 처리되어야 한다.
    
    * JPQL
        * 테이블이 아닌 객체를 대상으로 검색하는 객체지향쿼리
        * JPQL은 엔티티 객체를 대상으로 쿼리
        * 테이블을 대상으로 쿼리를 짜지 않는다.
        * 엔티티를 대상으로 쿼리를 만든다.

---
+++ src/main/java/JpaMain.java
@@ -0,0 +1,30 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA");
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
