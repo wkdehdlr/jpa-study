67b304c969bd9623289e02797175d0a5c0998e67
wkdehdlr <wkdehdlr@gmail.com>
2020-03-08 17:06:32 +0900

    * 1차캐시에서 조회

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -12,17 +12,13 @@ public class JpaMain {
         tx.begin();
 
         try{
-//          비영속
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloDoik");
+//          디비 쿼리 날아감
+            Member member1 = em.find(Member.class, 1L);
 
-//          영속(실제 디비에 쿼리가 안날아감)회
-            em.persist(member);
-//          디비가 아닌 1차캐시에서 조
-            Member member1 = em.find(Member.class, 2L);
+//          디비 쿼리 안날아감
+//          1차캐시에서 조회
+            Member member2 = em.find(Member.class, 1L);
 
-//          실제 디비에 쿼리가 날아감
             tx.commit();
         }catch (Exception e){
             tx.rollback();
