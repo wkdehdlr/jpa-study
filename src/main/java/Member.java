38ebff2d20680a2e59dc4ff2b0fb2b77f0b39a68
wkdehdlr <wkdehdlr@gmail.com>
2020-03-15 13:44:10 +0900

    * 엔티티 매핑     * @Entity         * 기본 생성자 필수(파라미터가 없는 public 또는 protected 생성자)         * final 클래스, enum, interface, inner 클래스 사용불가         * 저장할 필드에 final 사용불가         * 속성 : name             * 기본값 : 클래스 이름을 그대로 사용     * @Table         * 엔티티와 매핑할 테이블 지정         * name : 매핑할 테이블 이름 / 기본값 : 엔티티 이름을 사용
    * 데이터베이스 스키마 자동 생성
        * 운영 장비에는 절대 create, create-drop, update 사용하면 안된다.
        * 개발 초기 단계는 create 또는 update
        * 테스트 서버는 update 또는 validate
        * 스테이징과 운영 서버는 validate 또는 none
    
    * 필드와 컬럼 매핑
        * 매핑 어노테이션
            * @Column
                * name : 필드와 매핑할 테이블의 컬럼 이름
                * insertable, updatable : 등록 변경 가능 여부
                * nullable : null 값의 허용 여부. false로 하면 DDL 생성시에 not null 제약조건이 붙는다
                * unique : 컬럼에 unique를 붙이면 이름이 이상하게 만들어지기 때문에 이름을 지정할 수 있는 방법인 @Table(uniqueConstraints를 선호
                * columnDefinition : 데이터 베이스 컬럼 정보를 직접 줄 수 있음
                * length : 문자 길이 제약조건, String 타입에만 사용
                * precision, scale : BigDecimal 타입에서 사용
            * @Temporal : 날짜
                * LocalDate, LocalDateTime을 쓰면 안써도 된다
            * @Enumerated : enum
                * EnumType.ORDINAL : 디폴트, enum순서(정수)를 DB에 저장
                * EnumType.STRING : enum이름(String)을 DB에 저장
            * @Lob : BLOB, CLOB
                * 지정할 속성이 없다
                * 매핑할 타입이 문자면 CLOB, 나머지는 BLOB
            * @Transient : 매핑안함

---
+++ src/main/java/Member.java
@@ -0,0 +1,88 @@
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "MBR")
+public class Member {
+
+    @Id
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getLastModifedDate() {
+        return lastModifedDate;
+    }
+
+    public void setLastModifedDate(Date lastModifedDate) {
+        this.lastModifedDate = lastModifedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifedDate;
+
+    @Lob
+    private String description;
+
+    public Member(){
+
+    }
+
+}
---
+++ src/main/java/RoleType.java
@@ -0,0 +1,3 @@
+public enum RoleType {
+    USER, ADMIN
+}
