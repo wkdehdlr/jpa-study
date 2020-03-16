962da7d1ccc057c126103a87e88817d51ca5f185
wkdehdlr <wkdehdlr@gmail.com>
2020-03-17 08:35:28 +0900

    * 양방향 연관관계와 연관관계의 주인     * 연관관계의 주인과 mappedBy     * 객체의 양방향 관계         * 객체의 양방향 관계는 사실 양방향 관계가 아니라 서로 다른 단방향 관계 2개다     * 테이블의 양방향 관계         * 테이블은 외래 키 하나로 두 테이블의 연관관계를 관리     * 연관관계의 주인         * 양방향 매핑 규칙             * 객체의 두 관계중 하나를 연관곤계의 주인으로 지정             * 연관관계의 주인만이 외래 키를 관리(등록, 수정)             * 주인이 아닌쪽은 읽기만 가능             * 주인은 mappedBy 속성 사용 X             * 주인이 아니면 mappedBy 속성으로 주인 지정     * 누구를 주인으로?         * 외래 키가 있는 곳을 주인으로 지정

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -2,6 +2,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args){
@@ -20,8 +21,15 @@ public class JpaMain {
             member.setTeam(team);
             em.persist(member);
 
+            em.flush();
+            em.clear();
+
             Member member1 = em.find(Member.class, member.getId());
-            Team team1 = member1.getTeam();
+            List<Member> members = member1.getTeam().getMembers();
+
+            for(Member m : members){
+                System.out.println("m = " + m.getName());
+            }
 
 
             tx.commit();
--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,6 +1,9 @@
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -8,6 +11,9 @@ public class Team {
     @GeneratedValue
     private Long id;
     private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -23,5 +29,13 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
