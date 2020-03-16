4494536e8e8aa2824823a6993a89c4603e73d978
wkdehdlr <wkdehdlr@gmail.com>
2020-03-17 07:42:36 +0900

    * 연관관계 매핑기초

---
+++ src/main/java/Team.java
@@ -0,0 +1,11 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+}
