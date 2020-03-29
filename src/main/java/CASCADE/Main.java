857c9a0020ec3a7c78ba3c2f2ac61ba89e4ea7b8
wkdehdlr <wkdehdlr@gmail.com>
2020-03-29 18:30:57 +0900

    ### 영속성 전이 : CASCADE - 특정 엔티티를 영속 상태로 만들 때 연관된 엔티티 함께 영속 상태로 만들고 싶을 때

---
+++ src/main/java/CASCADE/Main.java
@@ -0,0 +1,35 @@
+package CASCADE;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("member");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        try{
+
+            Child child1 = new Child();
+            Child child2 = new Child();
+
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            em.persist(parent);
+
+            tx.commit();
+        }catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
