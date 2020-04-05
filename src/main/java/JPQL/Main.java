489eeac8ef046311ce6632fbd1f05198f4a7eec2
wkdehdlr <wkdehdlr@gmail.com>
2020-04-05 19:01:55 +0900

    - 3.new 명령어로 조회         - 단순 값을 DTO로 바로 조회         - 패키지 명을 포함한 전체 클래스 명 입력         - 순서와 타입이 일치하는 생성자 필요

--- src/main/java/JPQL/Main.java
+++ src/main/java/JPQL/Main.java
@@ -19,19 +19,22 @@ public class Main {
             em.flush();
             em.clear();
 
-            List resultList = em.createQuery("select m.address from Order m")
-                    .getResultList();
-            Object o = resultList.get(0);
-            Object[] res = (Object[])o;
-            System.out.println(res[0]);
-            System.out.println(res[1]);
+//            List resultList = em.createQuery("select m.address from Order m")
+//                    .getResultList();
+//            Object o = resultList.get(0);
+//            Object[] res = (Object[])o;
+//            System.out.println(res[0]);
+//            System.out.println(res[1]);
+//
+//            List<Object[]> resultList2 = em.createQuery("select m.address from Order m")
+//                    .getResultList();
+//
+//            Object[] result = resultList2.get(0);
+//            System.out.println(result[0]);
+//            System.out.println(result[1]);
 
-            List<Object[]> resultList2 = em.createQuery("select m.address from Order m")
+            List<Member> resultList = em.createQuery("select new JPQL.MemberDTO(m.username, m.age) FROM Member m", Member.class)
                     .getResultList();
-
-            Object[] result = resultList2.get(0);
-            System.out.println(result[0]);
-            System.out.println(result[1]);
 
             tx.commit();
         }catch (Exception e){
---
+++ src/main/java/JPQL/MemberDTO.java
@@ -0,0 +1,25 @@
+package JPQL;
+
+public class MemberDTO {
+    private String username;
+    private int age;
+
+    public MemberDTO() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
